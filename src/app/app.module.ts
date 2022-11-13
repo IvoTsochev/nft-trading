276cda535e53bcb3f3ff8b3f37c0d31a8a56bb6d
Ivaylo Tsochev <ivo.tsochev@live.com>
2022-11-13 18:24:00 +0200

    working on register and login modal - UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { CollectionComponent } from './collection/collection.component';
-import { IndexComponent } from './index/index.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {NavComponent} from './nav/nav.component'
+import {AboutUsComponent} from './about-us/about-us.component'
+import {CollectionComponent} from './collection/collection.component'
+import {IndexComponent} from './index/index.component'
+import {LoginComponent} from './login/login.component'
+import {SignUpComponent} from './sign-up/sign-up.component'
+import {UserModule} from './user/user.module'
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, AboutUsComponent, CollectionComponent, IndexComponent, LoginComponent, SignUpComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    AboutUsComponent,
+    CollectionComponent,
+    IndexComponent,
+    LoginComponent,
+    SignUpComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
